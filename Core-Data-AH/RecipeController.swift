0da307a1872119b58a728804e0b9f6946a9b5a85
AustinPotts <pottsevolvements@gmail.com>
2019-09-17 20:57:30 -0400

    Configured Model Controller (Recipe Controller)

---
+++ Core-Data-AH/RecipeController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeController.swift
+//  Core-Data-AH
+//
+//  Created by Austin Potts on 9/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class RecipeController {
+    
+    //MARK: - Methods
+    
+    
+    func createRecipe(title: String, cuisine: String, directions: String) -> Recipe {
+        let recipe = Recipe(title: title, cuisine: cuisine, directions: directions, context: CoreDataStack.share.mainContext)
+        
+        CoreDataStack.share.saveToPersistentStore()
+        
+        return recipe
+        
+    }
+    
+    func deleteRecipe(recipe: Recipe) {
+        CoreDataStack.share.mainContext.delete(recipe)
+        CoreDataStack.share.saveToPersistentStore()
+    }
+    
+    
+    
+    
+}
