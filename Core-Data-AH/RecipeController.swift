f0b7e84d9edcae6e0567ffc24c483b075e82dcb4
AustinPotts <pottsevolvements@gmail.com>
2019-09-21 20:03:32 -0400

    Configured Data Model Controller Put Method

--- Core-Data-AH/RecipeController.swift
+++ Core-Data-AH/RecipeController.swift
@@ -7,11 +7,61 @@
 //
 
 import Foundation
+import CoreData
 
+enum HTTPMethod: String{
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
 
 class RecipeController {
     
     //MARK: - Methods
+    
+    let fireBaseURL = URL(string: "https://recipes-e7b18.firebaseio.com/")!
+    
+    func put(recipe: Recipe, completion: @escaping()-> Void = {}) {
+        
+        let identifer = recipe.identifier ?? UUID()
+        recipe.identifier = identifer
+        
+        let requestURL = fireBaseURL.appendingPathComponent(identifer.uuidString).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        guard let recipeRepresentation = recipe.recipeRepresentation else{
+            NSLog("Error")
+            completion()
+            return
+        }
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(recipeRepresentation)
+        } catch {
+            NSLog("Error Encoding Rep: \(error)")
+            completion()
+            return
+            
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            if let error = error{
+                NSLog("Error putting task: \(error)")
+                completion()
+                return
+            }
+            completion()
+            }.resume()
+        
+        
+    }
+    
+    
+    
+    
     
     
     @discardableResult func createRecipe(title: String, cuisine: String, directions: String) -> Recipe {
